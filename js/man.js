36632a7e4c10091d07d98c659ae293267d157155
F1rdavs5767 <firdavsistamow@gmail.com>
2024-05-26 00:33:21 +0500

    Initial commit

---
+++ js/man.js
@@ -0,0 +1,44 @@
+var oneTest = prompt("Kompyuter tarmog`i nima?")
+var twoTest = prompt("Axborot resursi ta’rifini bering.")
+var threeTest = prompt("Internet nima?")
+var fourTest = prompt("Internet kimga, qaysi tashkilotga qarashli?")
+var fiveTest = prompt("Internetning negizida nima yotadi?")
+var sixTest = prompt("LAN va WAN nima?")
+var sevenTest = prompt("Tarmoq topologiyasi nima?")
+var eightTest = prompt("Xab (hub) nima?")
+var nineTest = prompt("Axborotning eng kichik o‘lchov birligi nima?")
+var tenTest = prompt("WLAN nima?")
+var elevenTest = prompt("Modemning vazifasi…")
+var twelveTest = prompt("Birinchi xisoblash vositalari.")
+var thirteenTest = prompt("info@tuit.uz elektron pochta manzilidagi «info» so‘zi nimani anglatadi?")
+var fourteenTest = prompt("Bir kilobayt qancha baytga teng?")
+var fifteenTest = prompt("Microsoft Word – bu:")
+var sixteenTest = prompt("Microsoft Word dasturi ma’lumotnomasini chaqirish qaysi tugma orqali amalga oshiriladi")
+var seventeenTest = prompt("Bir bayt necha bitga teng")
+var eighteenTest = prompt("Elektron xisoblash mashinalarini yaratilishiga xissa qo‘shgan birinchi rus olimi")
+var nineteenTest = prompt("Qaysi printer chop etish quvvati bo‘yicha eng tezkor hisoblanadi?")
+var twentyTest = prompt("CD qanday disklar toifasiga kiradi?")
+
+var result = {
+    oneTest:oneTest,
+    twoTest:twoTest,
+    threeTest:threeTest,
+    fourTest:fourTest,
+    fiveTest:fiveTest,
+    sixTest:sixTest,
+    sevenTest:sevenTest,
+    eightTest:eightTest,
+    nineTest:nineTest,
+    tenTest:tenTest,
+    elevenTest:elevenTest,
+    twelveTest:twelveTest,
+    thirteenTest:thirteenTest,
+    fourteenTest:fourteenTest,
+    fifteenTest:fifteenTest,
+    sixteenTest:sixteenTest,
+    seventeenTest:seventeenTest,
+    eighteenTest:eighteenTest,
+    nineteenTest:nineteenTest,
+    twentyTest:twentyTest,
+}
+console.log(result);
